127ee8c8826358d074b1a5bffb3f7687069695b4
kobiregev <regev79@gmail.com>
2023-01-15 15:59:57 +0200

    feat: with gzip

--- src/app.ts
+++ src/app.ts
@@ -4,19 +4,20 @@ import cors from "cors";
 import logger from "./utils/logger";
 import videoRoute from "./modules/videos/video.route";
 import helmet from "helmet";
+import compression from "compression";
 
-dotenv.config()
+dotenv.config();
 
 const signals = ["SIGTERM", "SIGINT"];
 const PORT = process.env.PORT || 4000;
-const {SECRET_KEY} = process.env;
+const { SECRET_KEY } = process.env;
 
 const app = express();
 
+app.use(compression());
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
-
 
 app.use("/api/videos", videoRoute);
 
